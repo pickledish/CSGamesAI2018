b5db484b9859892ec400ef94d30489b8d3f644e9
Brandon Willett <bcwillett1@gmail.com>
2018-03-24 11:14:38 -0400

    DAY TWO

--- src/bot/MyBot.py
+++ src/bot/MyBot.py
@@ -1,6 +1,7 @@
 from src.bot.Bot import Bot
 import networkx as nx
 from src.symbols.ObjectSymbols import ObjectSymbols
+from src.utils.Pathfinder import Pathfinder
 
 
 class MyBot(Bot):
@@ -10,7 +11,7 @@ class MyBot(Bot):
 
     def get_name(self):
         # Find a name for your bot
-        return 'My bot'
+        return 'UNIVERSITY ROCHESTER TEAM BEE GREATEST BOT VICTORY TEAM YES'
 
     def moveTowardsTreasure(self, game_state, character_state, other_bots):
 
@@ -102,6 +103,49 @@ class MyBot(Bot):
         return direction
 
 
+    def existsAPerson(self, game_state, character_state, other_bots, d):
+
+        currentLocation = character_state['location']
+        if (d == 'S'):
+            attemptedMove = (currentLocation[0] + 1, currentLocation[1])
+        elif (d == 'W'):
+            attemptedMove = (currentLocation[0], currentLocation[1] - 1)
+        elif (d == 'E'):
+            attemptedMove = (currentLocation[0], currentLocation[1] + 1)
+        else:
+            attemptedMove = (currentLocation[0] - 1, currentLocation[1])
+
+        for bot in other_bots:
+
+            botLoc = bot['location']
+            if (botLoc == attemptedMove):
+                return bot
+
+        return None
+
+    def shouldAttack(self, game_state, character_state, other_bots):
+
+        for direction in ['N', 'W', 'E', 'S']:
+            possibleBot = self.existsAPerson(game_state, character_state, other_bots, direction)
+            if (possibleBot and possibleBot['health'] <= 10):
+                return direction
+
+        return None
+
+    def getBaseDirection(self, game_state, character_state, other_bots, spikesToWalls):
+
+        p = Pathfinder()
+
+        if spikesToWalls:
+            game_state = game_state.replace("S", "1")
+
+        p.set_game_state(game_state, other_bots)
+
+        base = character_state['base']
+        currentLoc = character_state['location']
+        direction = p.get_next_direction(currentLoc, base)
+        return direction
+
     def turn(self, game_state, character_state, other_bots):
 
         super().turn(game_state, character_state, other_bots)
@@ -121,26 +165,42 @@ class MyBot(Bot):
         
         holding = (currentlyCarrying > 0)
 
-        enemyDist = self.nearestDanger(game_state, character_state, other_bots)
-        path = nx.astar_path(graph, enemyDist, character_state['location'])
+        enemyLoc = self.nearestDanger(game_state, character_state, other_bots)
+        path = nx.astar_path(graph, enemyLoc, character_state['location'])
         enemyIsNear = (len(path) < 4)
+        possibleAttackDirection = self.shouldAttack(game_state, character_state, other_bots)
 
-        if (holding and standingOnBase):
-            return self.commands.store()
-        if (standingOnBase and currentHealth < 90):
-            return self.commands.rest()
-        if (not holding and not standingOnTreasure):
-            d = self.moveTowardsTreasure(game_state, character_state, other_bots)
-            return self.commands.move(d)
-        if (standingOnTreasure and enemyIsNear or currentlyCarrying > 500):
-            base = character_state['base']
-            direction = self.pathfinder.get_next_direction(character_state['location'], base)
-            return self.commands.move(direction)
-        if (standingOnTreasure and not enemyIsNear):
-            return self.commands.collect()
-        if (holding):
-            base = character_state['base']
-            direction = self.pathfinder.get_next_direction(character_state['location'], base)
-            return self.commands.move(direction)
+        try:
+
+            if (holding and standingOnBase):
+                return self.commands.store()
+            if (standingOnBase and currentHealth < 90):
+                return self.commands.rest()
+            if (possibleAttackDirection):
+                return self.commands.attack(possibleAttackDirection)
+            if (currentHealth < 16 and holding and not standingOnTreasure):
+                return self.commands.move(self.getBaseDirection(game_state, character_state, other_bots, True))
+            if (not holding and not standingOnTreasure):
+                direc = self.moveTowardsTreasure(game_state, character_state, other_bots)
+                if self.existsAPerson(game_state, character_state, other_bots, direc):
+                    return self.commands.attack(direc)
+                else:
+                    return self.commands.move(direc)
+            if (standingOnTreasure and enemyIsNear or currentlyCarrying > 500):
+                return self.commands.move(self.getBaseDirection(game_state, character_state, other_bots, False))
+            if (standingOnTreasure and not enemyIsNear):
+                return self.commands.collect()
+            if (holding):
+                return self.commands.move(self.getBaseDirection(game_state, character_state, other_bots, False))
+            
+            return self.commands.idle()
+
+        except:
+            return self.commands.idle()
+
+
+        
+
+
 
 
